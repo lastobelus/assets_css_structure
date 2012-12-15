74a0e9d535a6247841fcb7cac4d0e0d967a27907
Michael Johnston <lastobelus@mac.com>
2012-12-14 17:38:59 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AssetsCssStructure::Application.config.secret_token = 'cac6c6412f7483c42ae7a5872678605259e01130754b3be405cb36ea8e85c73feb009163bc028d187ffd0fbf9358f5ebea844c8e989385a3bb2b822eda0e4647'
